68302f657297ddced3167cdf0d1f773a6d9b3907
Lucio Rivas <lucior0900@gmail.com>
2025-04-19 16:54:22 -0300

    suma y resta

--- funciones.py
+++ funciones.py
@@ -10,3 +10,28 @@ def multiplicar_binarios(binario1, binario2):
     for resultado in resultados:
         total = sumar_binarios(resultado, total)
     return total
+
+def sumar_binarios():
+    a = input("Ingresá el primer número binario: ")
+    b = input("Ingresá el segundo número binario: ")
+    resultado = bin(int(a, 2) + int(b, 2))[2:]
+    print(f"La suma es: {resultado}")
+
+def restar_binarios():
+    a = input("Ingresá el primer número binario: ")
+    b = input("Ingresá el segundo número binario: ")
+    resultado = bin(int(a, 2) - int(b, 2))[2:]
+    print(f"La resta es: {resultado}")
+
+def operacionSumaResta():
+    a = input("Primer número binario: ")
+    b = input("Segundo número binario: ")
+
+    suma = bin(int(a, 2) + int(b, 2))[2:]
+    resta = bin(int(a, 2) - int(b, 2))[2:]
+
+    print(f"Suma: {suma}")
+    print(f"Resta: {resta}")
+
+
+operacionSumaResta()
