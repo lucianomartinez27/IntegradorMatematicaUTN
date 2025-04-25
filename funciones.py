cb433e252311a479eaf79d6079bc1af7c78849d4
Santiago <santiii.ssg@gmail.com>
2025-04-25 18:15:00 -0300

    Cambios de codigo 'division binaria'

--- funciones.py
+++ funciones.py
@@ -23,6 +23,7 @@ def operacionSumaResta():
     resta = restar_binarios(a, b)
 operacionSumaResta()
 
+#///////////////////// MULTIPLICACION //////////////////////////
 
 def multiplicar_binarios(binario1, binario2):
     resultados = []
@@ -37,15 +38,9 @@ def multiplicar_binarios(binario1, binario2):
         total = sumar_binarios(resultado, total)
     return total
 
-#////////////////////////////////////////////////////////////////
+#///////////////////////// DIVISION //////////////////////////////
 
-def dividir():
-    # division de numeros binarios
-
-    # Pido dos numeros binarios como str
-    num_bin1 = input('Ingrese un numero decimal: ')
-    num_bin2 = input('Ingrese otro numero decimal: ')
-
+def dividir_binarios(num_bin1, num_bin2):
     # Convierto de binario a decimal
     num_dec1 = int(num_bin1, 2) # Pongo el ,2 para indicar que el numero que estamos pasando esta en base 2, osea binario.
     num_dec2 = int(num_bin2, 2)
@@ -53,6 +48,7 @@ def dividir():
     # Hago un if para evitar la división por cero
     if num_dec2 == 0:
         print('No se puede dividir por 0, intente con otro  numero')
+        return
 
     # Hago la division de los numeros decimales
     resultado_dec = num_dec1 // num_dec2 
@@ -61,11 +57,11 @@ def dividir():
     resultado_bin = bin(resultado_dec)[2:] # Pongo el [2:] para que omita los dos primeros caracteres del prefijo
     print(f'La divison binaria entre {num_bin1} y {num_bin2} es: {resultado_bin}')
 
+# Ingreso los parametros de la funcion definida
+dividir_binarios(input('Ingrese un numero binario: '), input('Ingrese otro numero binario: '))
 
+#////////////////////////// CONVERTIR ////////////////////////////////
 
-
-
-#////////////////////////////////////////////////////////////////
 def convertir_binario():
     while True:
         opcion = input("Elija la opción D para Decimal a Binario, o B para Binario a Decimal: ")
